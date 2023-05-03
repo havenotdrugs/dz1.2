381d2bbfcb8e7ce60aa62186d04c1307b79caab0
havenotdrugs <132453806+havenotdrugs@users.noreply.github.com>
2023-05-03 14:17:26 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+//Задание 1.2
+int main() {
+  int a;
+  cout << "Привет. Я возвожу в куб заданное тобой число.\nВведи число: ";
+  cin >> a;
+  cout << "Твое число в кубе - " << a * a * a;
+  return 0;
+}
